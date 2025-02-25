76a9318b2c89dcd2ead9b1b9bc4cd7d70f3e085b
Daniela Brunetto <dbrunetto.informatica@centralshop.com.py>
2025-02-25 16:38:31 -0300

    Pricing and FAQ sections advances

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import Header from './sections/Header.jsx'
 import Hero from './sections/Hero.jsx'
 import Features from './sections/Features.jsx'
+import Pricing from './sections/Pricing.jsx'
+import Faq from './sections/Faq.jsx'
+import Testimonials from './sections/Testimonials.jsx'
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <Header />
       <Hero />
       <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
     </main>
   )
 }
---
+++ src/sections/Testimonials.jsx
@@ -0,0 +1,15 @@
+import { Element } from "react-scroll";
+import { faq } from "../constants/index.jsx";
+import FaqItem from "../components/FaqItem.jsx";
+
+const Testimonials = () => {
+  const halfLength = Math.floor(faq.length / 2);
+
+  return (
+    <section className="relative z-2 py-24 md:py-28 lg:py-40">
+        <div></div>
+    </section>
+  );
+};
+
+export default Testimonials;
